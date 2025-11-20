04e2dc8634c890b911b151ed3c48b2fd9f815a71
Praveen1104 <106383728+Praveen1104@users.noreply.github.com>
2025-11-20 23:19:23 +0530

    this keword

--- this/thisKeyword.js
+++ this/thisKeyword.js
@@ -9,4 +9,89 @@ function call() {
   console.log(this);
 }
 
-call();
+call(); // browser this will return window
+
+
+//inside object    
+const user={
+    name:"praveen",
+    getDetails(){
+     console.log("Hi",this.name)
+    }
+
+}
+user.getDetails()
+
+const arrouser={
+    name:"praveen",
+    getdat:()=>{
+       console.log(this.name)
+    }
+}
+
+arrouser.getdat()
+
+
+//this in class  
+
+//this points the variable inside the constructor
+class users{
+constructor(n){
+    this.name=n
+}
+
+getName(){
+    console.log(this.name)
+}
+
+
+}
+const classuser=new users("bosco")
+classuser.getName()
+
+
+const firstuser={
+    firstName:'praveens',
+    getName(){
+        const firstName="p"
+        return this.firstName
+    }
+}
+console.log(firstuser.getName())
+
+
+//function=>object=>this =>  not going to point the current object
+
+function makeuser(){
+    return {
+        name:"bosco",
+        ref:this
+    }
+}
+
+let userf=makeuser()
+
+console.log(userf.ref)
+
+function makeuser1(){
+    return {
+        name:"bosco",
+        ref(){
+            return this
+        }
+    }
+}
+let userf1=makeuser1()
+
+console.log(userf1.ref().name)
+
+//set timeout version
+
+const windowuser={
+    name:"pb",
+    logmessage(){
+        console.log(this.name)
+    }
+}
+
+setTimeout(windowuser.logmessage,1000)
